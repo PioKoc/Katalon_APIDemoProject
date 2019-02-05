af308f8a52747398d5f4c7ff57928dfe0a25154a
Piotr.Kochanek.2 <piotr.kochanek.2@asseco.pl>
2019-02-05 13:26:05 +0100

    first project commit

---
+++ Test Suites/TS_ApiDemoProject.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_ApiDemoProject</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9430d278-094d-4e4f-943f-8cf3e68f65a8</testSuiteGuid>
+   <testCaseLink>
+      <guid>044c0e22-2309-4d6a-b73c-a0ddf07358b3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTests/Addition</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fcce7d04-3d4d-47e7-a27d-fa542b811684</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestHTTPMethods</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c1126b3a-c5fc-4c5b-8c82-e39b6e8c001b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2385dc3b-9d85-4e21-afe2-d1549b9c5b76</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7aa9a1b9-ffef-4b13-b957-0aeee665fac9</testSuiteGuid>
+   <testCaseLink>
+      <guid>780bd5fc-52b4-450c-ad2e-9ad436723869</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/CalculatorTests/Addition</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f12eeefa-4e04-4531-bd77-bec8bf13748e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>01476ccd-8cab-4770-9d3e-5160e50f3dfa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dc44286c-347d-4abe-9a3a-0d065d860572</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/TestHTTPMethods</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
